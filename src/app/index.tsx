5ca4c911a27940d312da5017c6264df8730457e6
mayaa-lves <mayara.alves.senai@gmail.com>
2026-09-15 13:12:26 -0300

    adiciona funcionalidade de ativação do microfone na tela inicial

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  function ativarMicrofone() {
+    console.log("Botão pressionado!");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>KARAOKAOS</Text>
+      <Text>Teste do microfone</Text>
+      <Pressable onPress={ativarMicrofone}>
+        <Text>ATIVAR MICROFONE</Text>
+      </Pressable>
+      <Text>Microfone ainda não iniciado</Text>
     </View>
   );
 }
